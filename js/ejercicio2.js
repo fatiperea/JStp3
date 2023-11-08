91fb621b8be2c1763f8550ac5f12daa2b00eef54
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-08 09:20:11 -0300

    agrego validaciones

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,32 +1,39 @@
 let i = 0,
   j = 0;
+let aux;
 const ciudades = [];
 do {
   ciudades[i++] = prompt("Ingrese ciudades: ");
 } while (confirm("Seguir ingresando?"));
 
-if (ciudades.length < 1) {
-  document.write(
-    "El arreglo ciudades tiene ",
-    ciudades.length + " elementos" + "<br>"
-  );
-  document.write("Primer elemento: ", ciudades[0] + "<br>");
-  document.write("Tercer elemento: ", ciudades[2] + "<br>");
-  document.write("Ultimo elemento: ", ciudades[ciudades.length - 1] + "<br>");
+if (ciudades.length > 0 && ciudades[0] !== null) {
+  for (j = 0; j < ciudades.length; j++) {
+    if (isNaN(ciudades[j])) aux = true;
+    else aux = false;
+  }
+  if (aux) {
+    document.write(
+      "El arreglo ciudades tiene ",
+      ciudades.length + " elementos" + "<br>"
+    );
+    document.write("Primer elemento: ", ciudades[0] + "<br>");
+    document.write("Tercer elemento: ", ciudades[2] + "<br>");
+    document.write("Ultimo elemento: ", ciudades[ciudades.length - 1] + "<br>");
 
-  ciudades.push("Paris");
+    ciudades.push("Paris");
 
-  document.write(
-    "Agrego elemento al final: ",
-    ciudades[ciudades.length - 1] + "<br>"
-  );
+    document.write(
+      "Agrego elemento al final: ",
+      ciudades[ciudades.length - 1] + "<br>"
+    );
 
-  document.write("Segundo elemento: ", ciudades[1] + "<br>");
+    document.write("Segundo elemento: ", ciudades[1] + "<br>");
 
-  ciudades.splice(1, 1, "Barcelona");
+    ciudades.splice(1, 1, "Barcelona");
 
-  document.write("Sustituyo segundo elemento: ", ciudades[1] + "<br>");
+    document.write("Sustituyo segundo elemento: ", ciudades[1] + "<br>");
 
-  document.write("<h4>Ciudades:<h4>");
-  for (j = 0; j < ciudades.length; j++) document.write(ciudades[j] + "<br>");
+    document.write("<h4>Ciudades:<h4>");
+    for (j = 0; j < ciudades.length; j++) document.write(ciudades[j] + "<br>");
+  } else document.write("No ingreso ciudades");
 } else document.write("No ingreso ciudades");
